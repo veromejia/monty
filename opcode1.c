e4e5aa52f13f36fbdb90ee473f9a817d8c611b37
veromejia <veromejia.q@gmail.com>
2019-09-26 11:55:58 -0700

    fixed nop

--- opcode1.c
+++ opcode1.c
@@ -4,7 +4,7 @@
  * @stack: Pointer to a doble linked list
  * @line_number: given line number
  */
-void nop(__attribute__((unused))stack_t **stack, __attribute__
-	 ((unused))unsigned int line_number)
+void nop(stack_t **stack, __attribute__((unused))unsigned int line_number)
 {
+	(void)stack;
 }
