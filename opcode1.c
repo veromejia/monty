9f1ad77df3a6f4810aa3a6df2fb8f8071b4af002
veromejia <veromejia.q@gmail.com>
2019-09-26 11:59:18 -0700

    fixed attribute unused nop function

--- opcode1.c
+++ opcode1.c
@@ -4,7 +4,8 @@
  * @stack: Pointer to a doble linked list
  * @line_number: given line number
  */
-void nop(stack_t **stack, __attribute__((unused))unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
+	(void)line_number;
 }
