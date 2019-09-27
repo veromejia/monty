9a12384ca12d3c731c19273af1263bb92ccff274
veromejia <veromejia.q@gmail.com>
2019-09-26 18:21:46 -0700

    fixed printing error

--- opcode1.c
+++ opcode1.c
@@ -82,12 +82,20 @@ void _div(stack_t **stack, unsigned int line_number)
 	stack_t *temp = *stack;
 	int len = dlistint_len(temp);
 
-	if (stack == NULL || *stack == NULL || len < 2 || temp->n == 0)
+	if (stack == NULL || *stack == NULL || len < 2)
 	{
 		dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n", line_number);
 		free(temp);
 		exit(EXIT_FAILURE);
 	}
+
+	if (temp->n == 0)
+	{
+		dprintf(STDERR_FILENO, "L%u: division by zero\n", line_number);
+		free(temp);
+		exit(EXIT_FAILURE);
+	}
+
 	temp->next->n /= temp->n;
 	pop(stack, line_number);
 }
